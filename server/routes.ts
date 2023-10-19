4f1e75e3307db97bbff03806ce247890a913283f
Angelina Zheng <ajz@mit.edu>
2023-10-19 16:59:24 -0400

    Fix bug in feed (logic error in thinking about initial feed)

--- server/routes.ts
+++ server/routes.ts
@@ -186,9 +186,9 @@ class Routes {
     const user = WebSession.getUser(session);
     const lastAuth = await User.getLastAuth(user);
     const feeds = await Feed.getByUser(user);
-    const lastFeed = feeds[0].displayFrom;
+    const lastFeed = feeds[0]?.displayFrom;
 
-    if (!lastFeed || lastFeed.getTime() < lastAuth.getTime()) {
+    if (!feeds || lastFeed.getTime() < lastAuth.getTime()) {
       const tiers = await Tier.getByOwner(user);
       const friends = await Friend.getFriends(user);
       const posts: ObjectId[] = [];
